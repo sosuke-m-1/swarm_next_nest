316fac870600b0bfb274e73e15245f9f9b850ab7
松下颯佑 <sosuke@matsushitasatsuyuunoMacBook-Pro.local>
2025-08-04 21:54:04 +0900

    日本語出力まで

--- backend/src/pdf/my-document.tsx
+++ backend/src/pdf/my-document.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
+import {
+    Document,
+    Page,
+    Text,
+    View,
+    StyleSheet,
+    Font,
+} from '@react-pdf/renderer';
+import path from 'path';
 
 const styles = StyleSheet.create({
     page: {
+        fontFamily: 'NotoSansJP',
+        fontWeight: 'bold',
         flexDirection: 'row',
         backgroundColor: '#E4E4E4',
         color: 'red',
@@ -10,6 +20,48 @@ const styles = StyleSheet.create({
         flexWrap: 'wrap',
     },
     section: { margin: 10, padding: 10, width: '100%', flexGrow: 1 },
+});
+
+Font.register({
+    family: 'NotoSansJP',
+    fonts: [
+        {
+            src: path.resolve(__dirname, '../fonts/NotoSansJP-Thin.ttf'),
+            fontWeight: 100,
+        },
+        {
+            src: path.resolve(__dirname, '../fonts/NotoSansJP-ExtraLight.ttf'),
+            fontWeight: 200,
+        },
+        {
+            src: path.resolve(__dirname, '../fonts/NotoSansJP-Light.ttf'),
+            fontWeight: 300,
+        },
+        {
+            src: path.resolve(__dirname, '../fonts/NotoSansJP-Regular.ttf'),
+            fontWeight: 400,
+        },
+        {
+            src: path.resolve(__dirname, '../fonts/NotoSansJP-Medium.ttf'),
+            fontWeight: 500,
+        },
+        {
+            src: path.resolve(__dirname, '../fonts/NotoSansJP-SemiBold.ttf'),
+            fontWeight: 600,
+        },
+        {
+            src: path.resolve(__dirname, '../fonts/NotoSansJP-Bold.ttf'),
+            fontWeight: 700,
+        },
+        {
+            src: path.resolve(__dirname, '../fonts/NotoSansJP-ExtraBold.ttf'),
+            fontWeight: 800,
+        },
+        {
+            src: path.resolve(__dirname, '../fonts/NotoSansJP-Black.ttf'),
+            fontWeight: 900,
+        },
+    ],
 });
 
 export const MyDocument = () => (
@@ -20,6 +72,7 @@ export const MyDocument = () => (
             </View>
             <View style={styles.section}>
                 <Text>PDF generation example.</Text>
+                <Text>あいうえお</Text>
             </View>
         </Page>
     </Document>
